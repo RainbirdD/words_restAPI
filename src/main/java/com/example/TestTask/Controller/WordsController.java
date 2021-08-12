fed7894087d7c4193c05e814e62e31ce9c827780
Oleg Belous <javelineua@gmail.com>
2021-08-12 11:06:37 +0300

    without tests

---
+++ src/main/java/com/example/TestTask/Controller/WordsController.java
@@ -0,0 +1,80 @@
+package com.example.TestTask.Controller;
+
+
+import org.springframework.web.bind.annotation.*;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("words")
+public class WordsController {
+    private int counter;
+
+
+    //Input??
+    ArrayList<String> arrayList = new ArrayList<>() {{
+        add("cat");
+        add("tom");
+        add("math");
+        add("hawk");
+        add("kill");
+        add("love");
+        add("rules");
+    }};
+
+    public Map<String, List> inputMap = new HashMap<>(){{
+        put("words", arrayList);
+    }};
+
+    @GetMapping("show")
+    public Map<String, List> showWords(){
+        return inputMap;
+    }
+
+
+    @GetMapping("filter")
+    public Map<String, List> filter(){
+
+        //new arr to put in map
+        ArrayList<String> newList = new ArrayList<>();
+
+        //not empty arr
+        if (!arrayList.isEmpty()) {
+            System.out.println(arrayList.get(0));
+            newList.add(arrayList.get(0));
+        } else
+            System.out.println(arrayList);
+
+        //main filter
+        for (int i = 0; i < arrayList.size(); i++) {
+
+
+            if (arrayList.get(i).charAt(arrayList.get(i).length() - 1) == arrayList.get(i + 1).charAt(0)
+                    &&
+                    arrayList.get(i + 1).charAt(0) == arrayList.get(i).charAt(arrayList.get(i).length() - 1)) {
+
+                newList.add(arrayList.get(i+1));
+
+                System.out.println(arrayList.get(i + 1));
+
+            } else
+                break;
+
+
+        }
+        inputMap.put("words", newList);
+        return inputMap;
+    }
+
+
+
+
+
+
+
+
+}
